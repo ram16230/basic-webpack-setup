b30834ec425e7c18c5dac7c66455c80514bc1181
javoz <javoz@localhost.localdomain>
2018-07-12 20:48:32 -0500

    prueba

--- src/js/client.js
+++ src/js/client.js
@@ -1,108 +1,40 @@
-import expect from 'expect';
-import { createStore } from 'redux';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Immutable from 'immutable';
+const state = {
+  player: [0, 1],
+  width: 3,
+  height: 3,
+  grid: {}
+};
 
-// Javascript the good parts
+const setup = lState => {
 
-const Counter = ({ value, incrementAction, decrementAction, removeAction }) => (
-  <div>
-    <h1>{ value }</h1>
-    <button onClick={ incrementAction }>+</button>
-    <button onClick={ decrementAction }>-</button>
-    <button onClick={ removeAction }>x</button>
-  </div>
-);
+  let celds = [];
 
-const CounterList = ({ list }) => (
-  <div>
-    {
-      list.map(
-        (value, i) => (
-          <Counter
-            key={ i }
-            value={ value }
-            index={ i }
-            incrementAction={
-              () => store.dispatch({
-                type: 'INCREMENT',
-                payload: { index: i }
-              })
-            }
-            decrementAction={
-              () => store.dispatch({
-                type: 'DECREMENT',
-                payload: { index: i }
-              })
-            }
-            removeAction={
-              () => store.dispatch({
-                type: 'REMOVE_COUNTER',
-                payload: {
-                  index: i
-                }
-              })
-            }
-          />
-        )
-      )
+  for (let j = 0; j < lState.height; j++){
+    celds[i] = []
+    for (let i = 0; i < lState.width; i++ ){
+      celds[i][j] = -1;
     }
-    <button onClick={ () => store.dispatch({ type: 'ADD_COUNTER' }) }>Add counter</button>
-  </div>
-);
-
-const validateIndex = (index, list) => 0 <= index && index < list.size;
-
-// Reducer
-const counterList = (state = Immutable.List.of(), action) => {
-
-  if(typeof action.payload !== 'undefined'){
-    var { index } = action.payload;
   }
 
-  switch(action.type){
-    case 'ADD_COUNTER':
-      return state.push(0);
+  console.log(celds);
 
-    case 'REMOVE_COUNTER':
+  root.appendChild(columns);
 
-      if(validateIndex(index, state)){
-        return state.delete(index);
-      }
+  console.log(lState.grid);
 
-      return state;
+  //contenedor del grid
+  const grid = document.createElement('div');
+  grid.className = 'grid';
 
-    case 'INCREMENT':
+  
+  
 
-      if(validateIndex(index, state)){
-        return state.update(index, (v) => v + 1);
-      }
-
-      return state;
-
-    case 'DECREMENT':
-
-      if(validateIndex(index, state)){
-        return state.update(index,  (v) => v - 1);
-      }
-
-      return state;
-
-    default:
-      return state;
-  }
+  root.appendChild(grid);
 }
 
-// createStore: reducer --> store
-const store = createStore(counterList);
+const render = lState => {
 
-const render = () => {
-  ReactDOM.render(
-    <CounterList list={ store.getState() } />,
-    document.getElementById('root')
-  )
 }
 
-store.subscribe(render);
-render();
+setup(state);
+render(state);
